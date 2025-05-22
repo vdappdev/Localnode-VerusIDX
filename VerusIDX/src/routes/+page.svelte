ce5a9a026adf17aaef139ae63f91f91e685f6f12
ejuliano <ejuliano@protonmail.com>
2025-05-21 23:35:06 -0400

    tauri built and running

---
+++ VerusIDX/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let name = 'VerusIDX Marketplace';
+</script>
+
+<main>
+  <h1>{name}</h1>
+  <p>Welcome to the Verus marketplace app!</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2em;
+    font-weight: 100;
+  }
+</style>
